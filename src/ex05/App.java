2ee10ca8ef7b76bfa04beb7cda66ba1dfcb11991
test231212 <j910219@gmail.com>
2024-05-28 11:01:10 +0900

    템플릿 메서드 패턴

---
+++ src/ex05/App.java
@@ -0,0 +1,10 @@
+package ex05;
+
+import ex05.teacher.HtmlTeacher;
+
+public class App {
+    public static void main(String[] args) {
+        HtmlTeacher jt = new HtmlTeacher();
+        jt.수업하기();
+    }
+}
